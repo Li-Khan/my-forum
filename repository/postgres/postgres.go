ebbe9024036bef8ca6b9982708d801c6fabdd87e
li-khan <alihanzh40@gmail.com>
2022-05-27 17:32:59 +0600

    deleted mistake.go

--- repository/postgres/postgres.go
+++ repository/postgres/postgres.go
@@ -6,28 +6,24 @@ import (
 	"time"
 
 	"github.com/Li-Khan/my-forum/config"
-	"github.com/Li-Khan/my-forum/lib/mistake"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 // * NewPostgresRepository - Connect to the database
-func NewPostgresRepository(c *config.Config) (db *pgxpool.Pool, err error) {
-	defer func() {
-		err = mistake.WrapIfErr("postgres", err)
-	}()
-
+func NewPostgresRepository(c *config.Config) (*pgxpool.Pool, error) {
 	dsn := fmt.Sprintf("postgres://%s:%s@%s%s/%s", c.UserDB, c.PasswordDB, c.HostDB, c.PortDB, c.NameDB)
+
 	cfg, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("postgres: %w", err)
 	}
 
 	cfg.MaxConns = 25
 	cfg.MaxConnLifetime = 5 * time.Minute
 
-	db, err = pgxpool.ConnectConfig(context.Background(), cfg)
+	db, err := pgxpool.ConnectConfig(context.Background(), cfg)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("postgres: %w", err)
 	}
 
 	return db, nil
